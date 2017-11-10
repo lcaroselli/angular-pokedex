d4b04b47268ed2ff7a660dc0c1b68c12f7a489bd
Laura Caroselli <lcaroselli@outlook.com>
2017-11-10 00:11:48 -0700

    Upload Pokexdex project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { HttpModule } from '@angular/http';
 
+import { PokedexService } from './services/pokedex.service';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CommonModule,
+    FormsModule,
+    HttpModule
   ],
-  providers: [],
+  providers: [PokedexService],
+  entryComponents: [AppComponent],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+
+}
